45123dba81cc29114f991d6bd3f96d73e702fed2
Ramya PC <ramya.pc@accoliteindia.com>
2020-08-24 17:39:19 +0530

    linked spring boot apps using an RestTemplate

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/Movie.java
@@ -0,0 +1,13 @@
+package io.javabrains.moviecatalogservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private String movieId;
+    private String name;
+}
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
@@ -1,23 +1,48 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("ABCD", "Ant body can dance", 4)
+
+       // RestTemplate restTemplate= new RestTemplate();
+
+        //get all rated movie ID's
+        List<Rating> ratings = Arrays.asList(
+                new Rating("Oh", 5),
+                new Rating("A", 4),
+                new Rating("OMO", 2),
+                new Rating("OMG", 3)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Movie desc", rating.getRating());
+        }).collect(Collectors.toList());
+
+//        return Collections.singletonList(
+//                new CatalogItem("ABCD", "Ant body can dance", 4)
+//        );
     }
 
 }
